5228bda9f826e28af5920518556e627bd21fadee
christopher-koleda <koleda.christopher@gmail.com>
2018-12-24 00:08:49 -0500

    Refactor filterer slightly

--- app/Filters/FIlterer.php
+++ app/Filters/FIlterer.php
@@ -8,41 +8,61 @@ use App\Filters\Contracts\Filter;
 
 class Filterer
 {
-    protected $builder;
-
-    protected $filters;
-
+    /**
+     * @var Illuminate\Http\Request;
+     */
     protected $request;
 
 
+    /**
+     * __construct
+     *
+     * @param Request $request
+     * @return void
+     */
     public function __construct(Request $request)
     {
         $this->request = $request;
     }
 
     /**
-     * Loop through all filters passed in,
+     * Loop through all applicable filters
      * call the apply method on each filter,
-     * passing in the query paramter aka key for each filter if it exists
+     * passing in the request parameter value
+     * for the given key
+     * return the builder with or without filters applied
      *
-     * return the builder after all filters are applied
-     * @param mixed $builder
+     * @param \Illuminate\Database\Eloquent\Builder $builder
+     * @param array $filters
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function apply($builder, $filters)
+    public function apply($builder, array $filters)
     {
-        foreach ($filters as $key => $filter) {
+        foreach ($this->siftFilters($filters) as $key => $filter) {
             if (!$filter instanceof Filter) {
                 throw new \Error('This filter must implement App\Filters\Contracts\Filter');
-            }
-            
-            if (!$this->request->has($key)) {
-                return ;
             }
 
             $filter->apply($builder, $this->request->get($key));
         }
 
         return $builder;
+    }
+
+    /**
+     * Iterate through the array of allowed filters
+     * return an aray of only filters whose keys match the
+     * query parameter keys from the request
+     *
+     * https://secure.php.net/manual/en/function.array-filter.php
+     *
+     * @param array $filters
+     * @return array
+     */
+    protected function siftFilters(array $filters) : array
+    {
+        return array_filter($filters, function ($key) {
+            return $this->request->has($key);
+        }, ARRAY_FILTER_USE_KEY);
     }
 }
